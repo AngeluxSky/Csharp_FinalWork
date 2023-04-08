9bffacf52a49e0e8103d820259333f6c477d0fe1
Kirill Ivanov <roxiegreen@yandex.ru>
2023-04-08 13:01:09 +0300

    Added code

---
+++ Program/Code/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = new string[4] {"1234", "1567", "-2", "computer science"};
+// string[] array = new string[4] {"Hello", "2", "world", ":-)"};
+// string[] array = new string[3] {"Russia", "Denmark", "Kazan"};
+string[] stringArray = new string[array.Length];
+void SecondStringArray(string[] array, string[] stringArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        stringArray[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondStringArray(array, stringArray);
+PrintArray(stringArray);
